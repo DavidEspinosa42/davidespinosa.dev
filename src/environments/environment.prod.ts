6ec230c0f8ef64d28a0ac13e13ecd4fd0840803c
David Espinosa <david.emanuel.espinosa@gmail.com>
2020-07-27 00:36:50 -0300

    Temporarely add mocks to prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { Environment } from './environment.interface';
 export const environment: Environment = {
 	production: true,
 	api: {
-		getBlogExcerpts: '',
-		getBlog: '',
+		getBlogExcerpts: 'assets/mocks/blog-excerpts-response.json',
+		getBlog: 'assets/mocks',
 	}
 };
