97230da77ed13ba516e8235a6e09d6c862ea248e
David Espinosa <david.emanuel.espinosa@gmail.com>
2020-08-17 21:47:06 -0300

    Set API env variables

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { Environment } from './environment.interface';
 export const environment: Environment = {
 	production: true,
 	api: {
-		getBlogExcerpts: 'https://h8ulfycge1.execute-api.us-east-1.amazonaws.com/dev/getBlogExcerpts',
-		getBlog: 'https://h8ulfycge1.execute-api.us-east-1.amazonaws.com/dev/getBlogPost',
+		getBlogExcerpts: 'https://vt10r2kmxj.execute-api.us-east-1.amazonaws.com/dev/getBlogExcerpts',
+		getBlog: 'https://vt10r2kmxj.execute-api.us-east-1.amazonaws.com/dev/getBlogPost',
 	}
 };
